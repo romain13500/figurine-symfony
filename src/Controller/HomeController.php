aa5748baf0d09f0b70c1ba76211d9bbf54b995ee
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-03-10 15:41:25 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FigurinesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,10 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function homepage(FigurinesRepository $figurinesRepository)
     {
-        return $this->render('home/homepage.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $figurines = $figurinesRepository->findAll();
+        dump($figurines);
+        return $this->render('home/homepage.html.twig');
     }
 }
