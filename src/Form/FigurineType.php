33130206e2b4658f381112c874ca728eaeff9a43
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-03-13 16:01:13 +0100

    Create figurineType
    php bin/console make:form FigurineController
    
    pour le formulaire

---
+++ src/Form/FigurineType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Figurines;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FigurineType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('picture')
+            ->add('price')
+            ->add('slug')
+            ->add('shortDescription')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Figurines::class,
+        ]);
+    }
+}
