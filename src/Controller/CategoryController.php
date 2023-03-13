38bb5fcc5dcb080c4bfc62248387182f0e7f873f
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-03-13 18:00:22 +0100

    navbar menu via BDD category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    protected $categoryRepository;
+    
+    public function __construct(CategoryRepository $categoryRepository)
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    #[Route('/category', name: 'app_category')]
+    public function renderMenuList() {
+        // 1. aller chercher les categorie dans la BDD (repository)
+        $categories = $this->categoryRepository->findAll();
+
+        // 2. renvoyer le rendu html sous forme de Response ($this->render)
+        return $this->render('category/_menu.html.twig', [
+            'categories' => $categories
         ]);
     }
 }
