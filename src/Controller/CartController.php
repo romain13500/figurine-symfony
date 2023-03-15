a5d2eabc1579b0795994c92b45148f6c69c24297
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-03-15 16:02:33 +0100

    add alert cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,15 +2,47 @@
 
 namespace App\Controller;
 
+use App\Repository\FigurinesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/cart/add/{id}', name: 'cart_add')]
-    public function add($id): Response
+
+    
+
+    
+    #[Route('/cart/add/{id}', name: 'cart_add', requirements: ['id' => '\d+'])]
+    public function add($id, FigurinesRepository $figurinesRepository, SessionInterface $session): Response
     {
-        dd($id);
+        $figurine = $figurinesRepository->find($id);
+        if(!$figurine) {
+            throw $this->createNotFoundException('La figurine n\'existe pas');
+        }
+
+        $cart = $session->get('cart', []);
+
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+
+        /** @var FlashBag */
+        $flashBag = $session->getBag('flashes');
+
+        $flashBag->add('success', 'La figurine a bien été ajoutée au panier');
+        
+        return $this->redirectToRoute('figurine_show', [
+            'category_slug' => $figurine->getCategory()->getSlug(),
+            'slug' => $figurine->getSlug()
+        ]);
     }
 }
