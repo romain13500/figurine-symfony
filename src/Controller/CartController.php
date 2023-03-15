abec62280fe42b5ee553a00db63c511cebc4437a
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-03-15 14:46:40 +0100

    cart controller

---
+++ src/Controller/CartController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/cart/add/{id}', name: 'cart_add')]
+    public function add($id): Response
+    {
+        dd($id);
+    }
+}
