4a4476b86042ce5eb12d5f535904d4804aa1788e
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-03-17 11:49:56 +0100

    create confirm controller

---
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Purchase;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class PurchaseConfirmationController {
+
+    #[Route('/purchase/confirm', name:'purchase_confirm')]
+    public function confirm()
+    {
+        
+    } 
+}
