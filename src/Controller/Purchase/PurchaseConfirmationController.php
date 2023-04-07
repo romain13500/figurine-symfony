5ae92a7363233366036b327aece27b82b0a52449
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-04-07 15:37:44 +0200

    payment page

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -66,9 +66,10 @@ class PurchaseConfirmationController extends AbstractController {
         $purchase = $form->getData();
 
         $this->persister->storePurchase($purchase);
-        $this->cartService->empty();
-        $this->addFlash('success', 'Votre commande a bien été enregistrée !');
-        return $this->redirectToRoute('purchase_index');
+        
+        return $this->redirectToRoute('purchase_payment_form', [
+            'id' => $purchase->getId()
+        ]);
 
     }
 }
--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller\Purchase;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class PurchasePaymentController extends AbstractController {
     
+    #[Route('/purchase/pay/{id}', name:'purchase_payment_form')]
+    public function showCardForm()
+    {
+        return $this->render('purchase/payment.html.twig');
+    }
 }
