c3b7a9c862565ddab978c8002f5ace3bc52cdb53
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-04-11 16:47:43 +0200

    payment

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Purchase;
 
+use App\Repository\PurchaseRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -9,8 +10,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PurchasePaymentController extends AbstractController {
     
     #[Route('/purchase/pay/{id}', name:'purchase_payment_form')]
-    public function showCardForm()
+    public function showCardForm($id, PurchaseRepository $purchaseRepository)
     {
-        return $this->render('purchase/payment.html.twig');
+        $purchase = $purchaseRepository->find($id);
+        if (!$purchase) {
+                return $this->redirectToRoute('cart_show');
+        }
+
+        \Stripe\Stripe::setApiKey('sk_test_51MnjngErconTUtbRWGXnIt6IQIiR6XccFKiDAstbz2meMD9nGSiHJcCm3J07LKvzoLaaJeKdMiYIhcgen2GLy1Or009ZeYqKTM');
+        
+        $intent = \Stripe\PaymentIntent::create([
+            'amount' => $purchase->getTotal(),
+            'currency' => 'eur'
+        ]);
+        
+        return $this->render('purchase/payment.html.twig', [
+           'clientSecret' => $intent->client_secret 
+        ]);
     }
 }
