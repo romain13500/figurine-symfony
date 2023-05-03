5b5da02d1b78d1a7d3fffba3e9d454e7ed57922b
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-05-03 16:59:42 +0200

    list purchase payement

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,16 +2,40 @@
 
 namespace App\Controller\Purchase;
 
+use App\Cart\CartService;
+use App\Form\CartConfirmationType;
+use App\Repository\FigurinesRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class PurchasePaymentController extends AbstractController {
     
+     /**
+     * @var CartService
+     */
+    protected $cartService;
+    protected $figurinesRepository;
+
+    public function __construct(FigurinesRepository $figurinesRepository, CartService $cartService)
+    {
+        $this->figurinesRepository = $figurinesRepository;
+        $this->cartService = $cartService;
+    }
+    
     #[Route('/purchase/pay/{id}', name:'purchase_payment_form')]
     public function showCardForm()
     {
-        
-        return $this->render('purchase/payment.html.twig');
+        $form = $this->createForm(CartConfirmationType::class);
+
+        $detailedCart = $this->cartService->detailedCartItems();
+
+        $total = $this->cartService->getTotal();
+
+        return $this->render('purchase/payment.html.twig', [
+            'items' => $detailedCart,
+            'total' => $total,
+            'confirmationForm' => $form->createView()
+        ]);
     }
 }
